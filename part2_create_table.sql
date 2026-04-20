403b824132d35c43b5bb888fba2924f513f96faa
Toolspack <88jayapandey17@gmail.com>
2026-04-20 17:47:17 +0530

    Add files via upload

---
+++ part2_create_table.sql
@@ -0,0 +1,21 @@
+#DB created in part1. If you have missed, check the video
+use school;
+-- 1.How to create the table 
+# Syntax 
+/*
+create table <tablename>
+(column1 datatype, 
+column2 datatype,.......
+column3 datatype)
+*/
+# Code
+create table student 
+(s_name varchar(100), #categorical column :varcar datatype
+s_city varchar(100),
+s_marks int,          # numerical column :int datatype
+s_age int      
+);
+-- 2.How to verify
+desc student;   #This will check the existence of the table in the database(school)
+
+
