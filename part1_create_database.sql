17060464e0554a81b1ba0ea35992148d46922c32
Toolpack123 <88jayapandey17@gmail.com>
2026-04-20 13:46:18 +0530

    Add files via upload

---
+++ part1_create_database.sql
@@ -0,0 +1,11 @@
+-- 1.Create A Database 
+# Syntax: 
+# create database <databasename>
+-- Code 
+create database school;
+
+-- 2. How to use this database
+# Syntax 
+# use <databasename>
+-- Code
+use school;
