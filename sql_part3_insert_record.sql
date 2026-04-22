3415f3f5e865e3357ccf7b702c0aaedca08f5fdd
Toolspack <88jayapandey17@gmail.com>
2026-04-22 10:18:03 +0530

    Update sql_part3_insert_record.sql

---
+++ sql_part3_insert_record.sql
@@ -0,0 +1,24 @@
+Use school; 
+# Covered in part1 and part2.
+# Database: school
+# Table: student
+# columns: s_name,s_class,s_city,s_marks
+-- ----------------------------------
+# Part 3: How to insert the record
+insert into student
+(s_name,s_class,s_city,s_marks)
+values
+('Raj',2,'Kanpur',80),
+('Ram',4,'luknow',100);
+
+# Display records
+select * from student;
+
+# Question for Practice
+-- 1. Create a database 'College'
+-- 2. Create two tables: a) Student and b) Teacher
+-- 3. Add column in Student table: S_id(int) , s_name(varchar1(100)), s_city(varchar(100)
+-- 4. Add column in Teacher table: T_id(int) , T_name(varchar1(100)), T_city(varchar(100)
+-- 5. Display Student and Teacher Table.
+
+# Kindly comment once it is done. 
