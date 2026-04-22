addb84bdf7f99e72dfe1be40c3c78466524562f0
Toolspack <88jayapandey17@gmail.com>
2026-04-22 18:40:57 +0530

    Add files via upload

---
+++ sql_part5_filtercolumns&Record.sql
@@ -0,0 +1,18 @@
+# Part5: Display specific records
+select * from student;
+
+select s_name, s_city, s_age
+from student 
+where s_age = 25;
+
+
+-- ------------------ Questions for Practice -------------------
+
+-- Q1. Write a query to display all students and their cities.
+
+-- Q2. Write a query to display only student names who live in Lucknow.
+
+-- ----------------------- HOTS Questions -----------------------
+-- (High Order Thinking Skills)
+-- Q3. Display students who belong to the age group of 25 and are from Lucknow city.
+-- Answer: Check next SQL part6 video " How to filter columns & multiple Records"
