dad948fc8155ebc6f14981afe9a9f1f57ad5f4cf
Toolspack <88jayapandey17@gmail.com>
2026-04-22 13:36:18 +0530

    Update sql_part4_whereClause.sql

--- sql_part4_whereClause.sql
+++ sql_part4_whereClause.sql
@@ -6,3 +6,9 @@ where condition
 # Display records only from kanpur city
 select * from student
 where s_city = 'kan';
+
+#----------------------------Questions for practice ------------------------------
+/*
+1. Display records only from 'Luknow' city
+2. Display only the records of 'Ram'
+*/
