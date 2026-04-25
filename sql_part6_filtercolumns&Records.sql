39c424f2a1d95e586ccdb79aead88fc34980453b
Toolspack <88jayapandey17@gmail.com>
2026-04-25 16:17:17 +0530

    Add files via upload

---
+++ sql_part6_filtercolumns&Records.sql
@@ -0,0 +1,19 @@
+/*Part6:Display students 
+who belong to the age 
+group of 25 and are 
+from Shimla city*/
+
+Use school;
+/*I want students' list 
+Condition1: age = 25 
+Condition2: city = Shimla*/
+
+select s_name from student 
+where s_age = 25 and
+s_city = 'Shimla';
+
+-- ----------------------------Practice Scenarios -------------------------
+# Display students who belong to the age group of 28 and are from Kanpur city
+
+
+
