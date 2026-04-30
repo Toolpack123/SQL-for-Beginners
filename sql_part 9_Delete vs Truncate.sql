84291457c5d8b203275c7ed0799aad9317197ae6
Toolspack <88jayapandey17@gmail.com>
2026-04-30 19:12:58 +0530

    Add files via upload

---
+++ sql_part 9_Delete vs Truncate.sql
@@ -0,0 +1,18 @@
+Use datascience;
+SELECT * FROM student;
+
+delete from student
+where Student_ID = 2;
+
+Truncate table student;
+
+-- -------------------------------Practice-------------------------
+/* 
+1. Create a database named Amazon.
+2. Create a table named product.
+3. Add the columns Product_Id, Product_Name, and Price.
+4. Insert 5 records ensuring each has a unique Product_Id.
+5. Delete a specific record using a condition.
+6. Delete all records from the table.
+*/
+
