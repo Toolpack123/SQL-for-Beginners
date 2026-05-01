990ab98dad8bd40e4e657b88c99e55be94137972
Toolspack <88jayapandey17@gmail.com>
2026-05-01 17:04:23 +0530

    Add files via upload

---
+++ sql_part10_Update_Where.sql
@@ -0,0 +1,21 @@
+# Part9 :UPDATE with WHERE
+/* 
+CREATE TABLE users (
+    user_id varchar(50) PRIMARY KEY,        -- Manual ID: 101, 102 etc.
+    user_name VARCHAR(50),
+    Category VARCHAR(20) 
+);
+INSERT INTO users (user_id, user_name, Category) 
+VALUES ('user1','Aariv', 'Education')
+('user2','Gopal', 'Artist');*/ 
+Desc users;
+select * from users;
+
+-- Scenario: changed category
+
+UPDATE users 
+SET Category = 'Digital creator'
+WHERE user_id = 'user1';
+
+-- 4. Verification
+SELECT * FROM users WHERE user_id = 'user1';
